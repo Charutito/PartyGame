0a04be0cd1f16cdcefaaae06309a2883b91fe8e4
JayLog22 <jaylog224@gmail.com>
2020-03-28 22:11:07 -0400

    [DEV] AAE, I want to research and implement some kind of command console for the client

---
+++ Assets/Scripts/GameUtils/Console/Commands/CommandQuit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Console
+{
+    public class CommandQuit : ConsoleCommand
+    {
+        public override string Name { get; protected set; }
+        public override string Command { get; protected set; }
+        public override string Description { get; protected set; }
+        public override string Help { get; protected set; }
+
+        public CommandQuit()
+        {
+            Name = "Quit";
+            Command = "quit";
+            Description = "Exit the application";
+            Help = "Use this command with no arguments to force Unity to quit the app.";
+
+            AddCommandToConsole();
+        }
+
+        public override void RunCommand()
+        {
+            if (Application.isEditor)
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        public static CommandQuit CreateCommand()
+        {
+            return new CommandQuit();
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameUtils/Console/ConsoleCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Console
+{
+    public abstract class ConsoleCommand
+    {
+        public abstract string Name { get; protected set; }
+        public abstract string Command { get; protected set; }
+        public abstract string Description { get; protected set; }
+        public abstract string Help { get; protected set; }
+
+        public void AddCommandToConsole()
+        {
+            string addMessage = " command has been added to the console.";
+            DeveloperConsole.AddCommandsToConsole(Command, this);
+            DeveloperConsole.AddMessageToConsole(Name + addMessage);
+        }
+
+        public abstract void RunCommand();
+
+    }
+}
